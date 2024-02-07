07b765c454a32a14f4d4b8592cc3d227b85d844f
SKOLZ <gabriel.zanzotti@gmail.com>
2024-02-07 11:42:04 +0100

    added vercel config and asset cms

--- src/App/pages/Photos/index.tsx
+++ src/App/pages/Photos/index.tsx
@@ -1,6 +1,34 @@
+import { useEffect } from "react";
 import styles from "./styles.module.scss";
 
 export const Photos: React.FC = () => {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://api-sa-east-1.hygraph.com/v2/clsbmingw2pio01w49t1h6the/master",
+      {
+        method: "POST",
+        headers: {
+          authorization: `bearer ${import.meta.env.VITE_HYGRAPH_TOKEN}`
+        },
+        body: JSON.stringify({query: `
+        {
+          assets(after: "clsbmjynt6bw50blu3qi3hxr5", first :2) {
+            createdAt
+            id
+            publishedAt
+            fileName
+            url
+            updatedAt
+          }
+        }
+      `})
+      });
+      const asd = await response.json();
+      console.log(asd);
+    };
+
+    fetchProducts();
+  }, []);
   return (
     <section className="page-container">
       <p className={styles.photosUnavailableText}>Fotos no disponibles temporalmente</p>
